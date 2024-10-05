a78933f342a1038a8d736a0a4351a35e5e70a6ea
Audreymary <audreymoraa006@gmail.com>
2024-10-05 22:30:45 +0300

    add tasklister project

---
+++ app.js
@@ -0,0 +1,42 @@
+let tasks = [];
+
+const addTask = ()=> {
+    const taskInput = document.getElementById('taskInput')
+    const text = taskInput.value.trim()
+
+    if (text) {
+        tasks.push({text: text, completed: false});
+        taskInput.value = "";
+        updateTasksList()
+    }
+};
+
+const updateTasksList = ()=> {
+    const taskList = document.getElementById('task-list')
+    taskList.innerHTML = ''
+
+    tasks.forEach(task => {
+        const listItem = document.createElement('li')
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task">
+              <input type="checkbox" class="checkbox" />
+              <p>finish this project</p>
+            </div>
+            <div class="icons">
+              <img src="./img/edit.png" />
+              <img src="./img/bin.png" />
+            </div>
+        </div>
+        `;
+
+        taskList.append(listItem)
+    })
+}
+
+document.getElementById('newTask').addEventListener('click', function(e){
+e.preventDefault();
+
+addTask();
+});
